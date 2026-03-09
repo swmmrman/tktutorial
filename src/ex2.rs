9a74be0509a73e60ec8de3e241c19435e3064e1b
Stephan Kern <stephan@swmmrman.com>
2026-03-09 00:37:09 -0600

    Math is wrong,  But its a working example 2

--- src/ex2.rs
+++ src/ex2.rs
@@ -5,7 +5,7 @@ use tk::*;
 fn main() -> TkResult<()> {
     let tk = make_tk!()?; //No error handing....
     let root = tk.root();
-    root.set_wm_title("C to F and back")?; // Again?
+    root.set_wm_title("mytks")?; // Again?
 
     let c = root
         .add_ttk_frame("c" - padding((3, 3, 12, 12)))? // Zed reformatted this
@@ -14,6 +14,47 @@ fn main() -> TkResult<()> {
     root.grid_columnconfigure(0, -weight(1))?; //Might be fun to add some
     root.grid_rowconfigure(0, -weight(1))?;
 
-    c.add_ttk_label("Temp F" - width(7), -textvariable("Temp F"))?
+    let tempc = c.add_ttk_entry("tempc" - width(7) -textvariable("tempc"))?
         .grid(-column(2) - row(1) - sticky("we"))?;
+
+    c.add_ttk_label("tempf" -textvariable("tempf"))?
+        .grid( -column(2) -row(2) -sticky("we"))?;
+
+    c.add_ttk_button( "calc" -text("Convert to °C") -command("calculate"))?
+        .grid( -column(3) -row(3) -sticky("w"))?;
+
+    c.add_ttk_label("flbl" -text("°F"))?
+        .grid( -column(3) -row(1) -sticky("w"))?;
+
+    c.add_ttk_label("eqlbl" -text("converts to"))?
+        .grid( -column(1) -row(2) -sticky("e"))?;
+
+    c.add_ttk_label("clbl" -text("°C"))?
+        .grid( -column(3) -row(2) -sticky("w"))?;
+
+    c.winfo_children()?
+        .iter()
+        .try_for_each( |child| child.grid_configure( -padx(5) -pady(5)))?;
+
+    tempc.focus()?;
+
+    #[proc]
+    fn calculate() -> TkResult<()> {
+        let interp = tcl_interp!();
+        let tempc = interp.get_double("tempc");
+        match tempc {
+            Ok(tempc) => {
+                let tempf = f64::floor( (tempc * 1.8) + 32.0 );
+                interp.set_double("tempf", tempf)
+            },
+            Err(_) => interp.set("tempf", ""),
+        };
+        Ok(())
+    }
+
+    //Tutorial says safe due to #[proc]
+    unsafe { tk.def_proc("calculate", calculate);}
+    root.bind_more( event::key_press(TkKey::Return), "calculate")?;
+
+    Ok(main_loop())
 }
